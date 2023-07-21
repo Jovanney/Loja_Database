cd2ad4ac69708146a61c45db2738279bccdf3ce5
Jovanney <100790418+Jovanney@users.noreply.github.com>
2023-07-21 13:44:44 -0300

    feat(CraiacaaoTabelas.sql) entidades descrição e protocolo atendimento

--- scripts/CriacaoTabelas.sql
+++ scripts/CriacaoTabelas.sql
@@ -9,7 +9,7 @@ CREATE TABLE Assistencia
 
     CONSTRAINT Assistencia_pkey PRIMARY KEY (Cnpj));
 
---Criando tabela TipoAssistencia
+--Criando tabela Tipo da Assistencia
 
 CREATE TABLE TipoAssistencia
    (Tipo_Assistencia VARCHAR2(50) NOT NULL,
@@ -17,3 +17,38 @@ CREATE TABLE TipoAssistencia
     
     CONSTRAINT TipoAssistencia_pkey PRIMARY KEY (Tipo_Assistencia),
     CONSTRAINT TipoAssistencia_fkey FOREIGN KEY (Cnpj_Assistencia) REFERENCES Assistencia(Cnpj));
+
+--Criando Tabela Protocolo de Atendimento e descrição com pk com autoincremento
+
+CREATE SEQUENCE descricao_seq;
+
+CREATE TABLE Descricao
+   (Descricao_n NUMBER NOT NULL,
+    Acoes_Tomadas VARCHAR2(50) NOT NULL,
+    
+    CONSTRAINT Descricao_pkey PRIMARY KEY (Descricao_n));
+
+CREATE TABLE Protocolo_de_Atendimento (
+    Codigo_Protocolo VARCHAR2(50),
+    Cnpj VARCHAR2(14),
+    Desc_Pro NUMBER,
+    Acoes_Tomadas VARCHAR2(50),
+    Data_Inicio DATE,
+    Data_Conclusao DATE,
+    
+    CONSTRAINT Protocolo_pk PRIMARY KEY (Codigo_Protocolo),
+    CONSTRAINT Desc_Pro_fk FOREIGN KEY (Desc_Pro) REFERENCES Descricao(Descricao_n),
+    CONSTRAINT Cnpj_fk FOREIGN KEY (Cnpj) REFERENCES Assistencia(Cnpj));
+
+CREATE OR REPLACE TRIGGER descricao_bir 
+
+BEFORE INSERT ON Descricao 
+FOR EACH ROW
+
+BEGIN
+  SELECT descricao_seq.NEXTVAL
+  INTO   :new.descricao_n
+  FROM   dual;
+END;
+
+
