12a5da457b67b623ba831d6d92108e6abe9c67c8
Jovanney <100790418+Jovanney@users.noreply.github.com>
2023-07-24 10:32:24 -0300

    feat(Consultas.sql) 2 consultas feitas

--- scripts/Consultas.sql
+++ scripts/Consultas.sql
@@ -10,3 +10,19 @@ WHERE pg.status LIKE 'Confirmado';
 SELECT COUNT(*) AS Sem_supervisor
 FROM Funcionario f
 WHERE f.cad_supv IS NULL;
+
+--Selecionando o Endereco e o Telefone dos usuários
+
+SELECT u.email, u.nome, e.bairro, e.rua, t.numero
+FROM Usuario u
+LEFT OUTER JOIN Endereco e
+ON u.cep = e.cep
+LEFT OUTER JOIN Telefone t
+ON u.email = t.email_usuario
+ORDER BY u.nome;
+
+--Selecionando todos os usuários que a idade é maior que a média da idade de todos os usuários
+
+SELECT u.email, u.nome, u.idade
+FROM Usuario u
+WHERE u.idade > (SELECT AVG(u2.idade) FROM Usuario u2);
